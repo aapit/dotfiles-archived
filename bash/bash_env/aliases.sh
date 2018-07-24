ac358dccf9ccf602e5039ff3422a3d2c3fad1516
David Spreekmeester <david@grrr.nl>
2018-07-24 14:18:34 +0200

    Add aliases for desk, sites cd, docker sync stack and git upstream

--- bash/bash_env/aliases.sh
+++ bash/bash_env/aliases.sh
@@ -1,9 +1,11 @@
 alias ls='/bin/ls -Ahl'
 alias src='. ~/.bashrc'
-alias composer='composer.phar'
+#alias composer='composer.phar'
 alias ..="cd .."
 alias ...="cd ../.."
 alias a="atom ."
+alias desk="cd ~/Desktop"
+alias s=". ~/Scripts/bash_env/site-dir-change.sh"
 
 # Capistrano
 alias csd='cap staging deploy'
@@ -17,9 +19,12 @@ alias gp='git push'
 alias ga='git add .'
 alias dev='git co develop'
 alias master='git co master'
-alias git-push-upstream='git push --set-upstream origin (git rev-parse --abbrev-ref HEAD)'
+alias git-push-upstream='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 
 # iTerm profile switching
 alias light='echo -e "\033]50;SetProfile=aapit-light\a"'
 alias dark='echo -e "\033]50;SetProfile=aapit-dark\a"'
 alias darkish='echo -e "\033]50;SetProfile=aapit-darkish\a"'
+
+# Docker
+alias dss='docker-sync-stack start'
