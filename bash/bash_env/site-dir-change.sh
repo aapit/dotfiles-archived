280f4e9d76f2063068a30538ebeea9503046a0ae
David Spreekmeester <david@grrr.nl>
2018-07-24 20:38:33 +0200

    Update site dir change script with smarter search

--- bash/bash_env/site-dir-change.sh
+++ bash/bash_env/site-dir-change.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
 # Change dir to one of the Sites dirs
+
+BASE_PATH=$HOME/Sites
 
 if [ "$1" = "" ]; then
     echo 'Which site dir?'
 else
-    cd $HOME/Sites/*$1*
+    THE_DIR=$(find $BASE_PATH -maxdepth 1 -type d -name "*$1*" -print -quit)
+    if [ "$THE_DIR" = "" ]; then
+        echo '404 not found, frowny face.'
+    else
+        cd $THE_DIR
+    fi
 fi
 
