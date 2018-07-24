0191dbb3b3b34bf5daf9d781e6c4b48e36ce26c0
David Spreekmeester <david@grrr.nl>
2018-07-25 00:44:25 +0200

    Add improvement on fuzzy cd

--- bash/bash_env/site-dir-change.sh
+++ bash/bash_env/site-dir-change.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # Change dir to one of the Sites dirs
+# by David Spreekmeester @aapit
 
 BASE_PATH=$HOME/Sites
 
 if [ "$1" = "" ]; then
     echo 'Which site dir?'
 else
-    THE_DIR=$(find $BASE_PATH -maxdepth 1 -type d -name "*$1*" -print -quit)
-    if [ "$THE_DIR" = "" ]; then
+    BEGINS_WITH="$1*"
+    CONTAINS="*$1*"
+    DIR_PATTERN="find $BASE_PATH -maxdepth 1 -type d -name \"%s\" -print -quit"
+    DIR_BEGINS_WITH=$(printf "$DIR_PATTERN" "$BEGINS_WITH")
+    FOUND_DIR=$(eval $DIR_BEGINS_WITH)
+
+    if [ "$FOUND_DIR" = "" ]; then
+        DIR_CONTAINS=$(printf "$DIR_PATTERN" "$CONTAINS")
+        FOUND_DIR=$(eval $DIR_CONTAINS)
+    fi
+
+    if [ "$FOUND_DIR" = "" ]; then
         echo '404 not found, frowny face.'
     else
-        cd $THE_DIR
+        cd $FOUND_DIR
     fi
 fi
 
