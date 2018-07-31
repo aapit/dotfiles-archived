394ac901afcf91dfd94d5932854a2b58bed4f308
David Spreekmeester <david@grrr.nl>
2018-08-01 00:43:23 +0200

    Update aliases
    * Add function key toggle
    * Restructured aliases
    * Restructured interface script dirs
    * Changed ls alias to GNU version, including coloring

--- bash/bash_env/aliases.sh
+++ bash/bash_env/aliases.sh
@@ -1,9 +1,12 @@
-alias ls='/bin/ls -Ahl'
+alias ls='LS_COLORS="no=00:fi=00:di=00;34:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=00;35:*.cmd=00;32:*.exe=00;32:*.sh=00;32:*.gz=00;31:*.bz2=00;31:*.bz=00;31:*.tz=00;31:*.rpm=00;31:*.cpio=00;31:*.t=93:*.pm=00;36:*.pod=00;96:*.conf=00;33:*.off=00;9:*.jpg=00;94:*.png=00;94:*.xcf=00;94:*.JPG=00;94:*.gif=00;94:*.pdf=00;91" /usr/local/opt/coreutils/libexec/gnubin/ls --group-directories-first -Ahl --color=tty'
+
 alias src='. ~/.bash_profile'
-#alias composer='composer.phar'
+alias a="atom ."
+alias alias-edit="vim ~/Scripts/bash_env/aliases.sh && . ~/Scripts/bash_env/aliases.sh"
+
+# Dirs
 alias ..="cd .."
 alias ...="cd ../.."
-alias a="atom ."
 alias desk="cd ~/Desktop"
 alias s=". ~/Scripts/bash_env/site-dir-change.sh"
 
@@ -22,8 +25,13 @@ alias master='git co master'
 alias git-push-upstream='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 
 # iTerm profile switching, tmux proof
-alias light='osascript ~/Scripts/switch-to-light.applescript'
-alias dark='osascript ~/Scripts/switch-to-dark.applescript'
+alias light='osascript ~/Scripts/interface/switch-to-light.applescript'
+alias dark='osascript ~/Scripts/interface/switch-to-dark.applescript'
+
+# Toggle function keys / Apple functions
+alias fn-off='osascript ~/Scripts/interface/function-keys-off.applescript'
+alias fn-on='osascript ~/Scripts/interface/function-keys-on.applescript'
+alias fn-toggle='osascript ~/Scripts/interface/function-keys-on.applescript'
 
 # Docker
 alias dss='docker-sync-stack start'
