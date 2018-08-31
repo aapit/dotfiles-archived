7bf4a37c9bd1c71ebeffd023262a7943ad1f57ba
David Spreekmeester <david@grrr.nl>
2018-08-31 18:16:15 +0200

    Update cleaned up HSTR config

--- bash/bash_env/hstr_configuration.sh
+++ bash/bash_env/hstr_configuration.sh
@@ -1,10 +1,15 @@
 # HSTR (Ctrl+R enhancer) configuration
-export HH_CONFIG=hicolor         # get more colors
-shopt -s histappend              # append new history items to .bash_history
-export HISTCONTROL=ignorespace   # leading space hides commands from history
-export HISTFILESIZE=10000        # increase history file size (default is 500)
-export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
-export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"   # mem/file sync
+export HH_CONFIG=hicolor
+# append new history items to .bash_history
+shopt -s histappend
+# leading space hides commands from history
+export HISTCONTROL=ignorespace
+# increase history file size (default is 500)
+export HISTFILESIZE=10000
+# increase history size (default is 500)
+export HISTSIZE=${HISTFILESIZE}
+# mem/file sync
+export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
 # if this is interactive shell, then bind hh to Ctrl-r (for Vi mode check doc)
 if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hh -- \C-j"'; fi
 # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
