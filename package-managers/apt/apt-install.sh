d2a3ee5de852d823d1378e4e6754b48200d01590
David Spreekmeester <david@grrr.nl>
2018-10-29 11:09:06 +0100

    Add improvements to apt automation
    * Prevent double repo fingerprint adding
    * Add new packages (nfs, exfat / fuse)
    * Fix typo for docker-compose package
    * Add auto-confirm for package installer

--- package-managers/apt/apt-install.sh
+++ package-managers/apt/apt-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SCRIPT_PATH="`dirname \"${BASH_SOURCE[0]}\"`"
 . ${SCRIPT_PATH}/apt-repos.sh
-sudo apt install $(/bin/cat ${SCRIPT_PATH}/apt-packages.txt)
+sudo apt install -y $(/bin/cat ${SCRIPT_PATH}/apt-packages.txt)
+sudo apt autoremove
--- package-managers/apt/apt-repos.sh
+++ package-managers/apt/apt-repos.sh
@@ -1,8 +1,11 @@
 sudo apt install curl
 
 # Signal for desktop repository
-curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+signalrepo="deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main"
+if ! grep -Fxq "$signalrepo" /etc/apt/sources.list.d/signal-xenial.list; then
+    curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+    echo "$signalrepo" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+fi;
 
 # Polo file manager
 sudo apt-add-repository -y ppa:teejee2008/ppa
