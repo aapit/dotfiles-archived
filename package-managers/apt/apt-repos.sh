4f48425522693337faa766b63351c7bde0e586d9
David Spreekmeester <david@grrr.nl>
2018-11-20 12:25:38 +0100

    Update repos and packages
    * Add pomodoro
    * Restructured custom repo adding

--- package-managers/apt/apt-repos.sh
+++ package-managers/apt/apt-repos.sh
@@ -1,10 +1,19 @@
 sudo apt install curl
 
 # Signal for desktop repository
-signalrepo="deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main"
-if ! grep -Fxq "$signalrepo" /etc/apt/sources.list.d/signal-xenial.list; then
+repo="deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main"
+listfile="/etc/apt/sources.list.d/signal-xenial.list"
+if ! grep -Fxq "$repo" $listfile; then
     curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-    echo "$signalrepo" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+    echo "$signalrepo" | sudo tee -a $listfile
+fi;
+
+# Pomodoro app
+repo="deb https://download.opensuse.org/repositories/home:/kamilprusko/xUbuntu_18.10/ /"
+listfile="/etc/apt/sources.list.d/gnome-pomodoro.list"
+if ! grep -Fxq "$repo" $listfile; then
+    curl -s https://download.opensuse.org/repositories/home:/kamilprusko/xUbuntu_18.10/Release.key | sudo apt-key add -
+    echo "$repo" | sudo tee -a $listfile
 fi;
 
 # Polo file manager
