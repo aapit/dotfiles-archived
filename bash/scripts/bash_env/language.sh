4c301750b0e31b1fcdc86dfac3f5c8d8bf1cbb28
David Spreekmeester <david@grrr.nl>
2019-06-03 11:26:28 +0200

    Add locale specific exceptions for money, date, etc

--- bash/scripts/bash_env/language.sh
+++ bash/scripts/bash_env/language.sh
@@ -1,3 +1,10 @@
 # Lang
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export LC_NUMERIC=en_NL.UTF-8
+export LC_TIME=en_NL.UTF-8
+export LC_MONETARY=en_NL.UTF-8
+export LC_PAPER=en_NL.UTF-8
+export LC_ADDRESS=en_NL.UTF-8
+export LC_MEASUREMENT=en_NL.UTF-8
+export LC_TELEPHONE=en_NL.UTF-8
