48a37e1837fed85d112c9491c5595bd68df5c271
David Spreekmeester <david@grrr.nl>
2019-07-12 02:55:05 +0200

    Fix powerline bash binding path

--- bash/scripts/bash_env/powerline.sh
+++ bash/scripts/bash_env/powerline.sh
@@ -2,4 +2,4 @@
 powerline-daemon -q 
 POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
-source $HOME/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
+source /usr/share/powerline/bindings/bash/powerline.sh
