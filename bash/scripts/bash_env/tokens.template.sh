d3435f915f91167c67424a991b0a5f3de8bef6ad
David Spreekmeester <david@grrr.nl>
2019-07-12 03:03:45 +0200

    Add template file for tokens shell config

---
+++ bash/scripts/bash_env/tokens.template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export TOKEN_TIME_CAPSULE_USER=
+export TOKEN_TIME_CAPSULE_PASS=
+export TOKEN_NAS_USER=
+export TOKEN_NAS_PASS=
+
+# Restic backups
+export RESTIC_REPOSITORY=
+export RESTIC_PASSWORD=
