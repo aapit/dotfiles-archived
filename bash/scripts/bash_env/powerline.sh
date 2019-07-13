c271433baff398eebb3b2a363fffe45cb4445c12
David Spreekmeester <david@grrr.nl>
2019-07-14 01:57:16 +0200

    Disabled Powerline daemon restart

--- bash/scripts/bash_env/powerline.sh
+++ bash/scripts/bash_env/powerline.sh
@@ -1,5 +1,5 @@
 # Powerline
-powerline-daemon -q 
-POWERLINE_BASH_CONTINUATION=1
-POWERLINE_BASH_SELECT=1
-source /usr/share/powerline/bindings/bash/powerline.sh
+#powerline-daemon -q 
+export POWERLINE_BASH_CONTINUATION=1
+export POWERLINE_BASH_SELECT=1
+export POWERLINE_CONFIG_COMMAND=powerline-config
