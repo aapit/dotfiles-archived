02442b901d3a1e7cf4e5cfe9db8f68c22d03232a
David Spreekmeester <david@grrr.nl>
2019-07-15 11:09:44 +0200

    Add colors and changed prefix color prompt

--- bash/scripts/bash_env/prompt.sh
+++ bash/scripts/bash_env/prompt.sh
@@ -1,14 +1,17 @@
+BLACK=$'\[\033[1;30m\]'
 WHITE=$'\[\033[1;37m\]'
 BLUE=$'\[\033[1;34m\]'
 GREEN=$'\[\033[0;32m\]'
+YELLOW=$'\[\033[0;33m\]'
 RED=$'\[\033[0;31m\]'
 GREY=$'\[\033[0;37m\]'
 DARKGREY=$'\[\033[0;30m\]'
 CYAN=$'\033[0;36m'
+MAGENTA=$'\033[0;35m'
 NONE=$'\[\033[m\]'
 
 source ~/Scripts/bash_env/prompt_helpers/nonzero_return.sh
 source ~/Scripts/bash_env/prompt_helpers/parse_git_branch.sh
 source ~/Scripts/bash_env/prompt_helpers/parse_git_dirty.sh
 
-export PS1="${DARKGREY}C:\\${CYAN}\W${RED}\`nonzero_return\`${GREY}\`parse_git_branch '1;34'\`  ${NONE}\n> "
+export PS1="${MAGENTA}C:\\${CYAN}\W${RED}\`nonzero_return\`${GREY}\`parse_git_branch '1;34'\`  ${NONE}\n> "
