9304140319a4a3d6a92b08505ab2f36efc2b5962
David Spreekmeester <david@grrr.nl>
2019-07-15 23:41:57 +0200

    Moved tmux path files out of .conf to startup sh

--- bash/scripts/bash_env/tmux.sh
+++ bash/scripts/bash_env/tmux.sh
@@ -1,2 +1,5 @@
+export TMUX_CONFIG_PATH=$HOME/.config/tmux/
+export TMUX_PLUGIN_MANAGER_PATH=$HOME/.config/tmux/plugins/
+
 [ -z "$TMUX" ] && export TERM=xterm-256color
 [ -z "$TMUX" ] && exec tmux new -As 家に
