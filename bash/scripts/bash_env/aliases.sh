e4618e607580c5ed09de8dd22669e36fc5db0004
David Spreekmeester <david@grrr.nl>
2019-08-09 20:30:29 +0200

    Add shortcut for editing vim config files

--- bash/scripts/bash_env/aliases.sh
+++ bash/scripts/bash_env/aliases.sh
@@ -77,6 +77,9 @@ alias icons-edit="$EDITOR ~/Scripts/replace-custom-icons.sh"
 alias apt-edit-pkgs="vim $HOME/Scripts/dotfiles/package-managers/apt/apt-packages.txt"
 alias apt-install-pkgs=". $HOME/Scripts/dotfiles/package-managers/apt/apt-install.sh"
 
+# Vim
+alias vimcfg="cd ~/.config/vim/config && vim . && cd -"
+
 # Spotify
 alias spotify="$(which spotify) --force-device-scale-factor=2 > /dev/null &"
 
